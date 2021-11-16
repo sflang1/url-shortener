c8b8dcf94f2d9c3becb640d5ae49d5576d01b595
Sebastián Landínez <slandinezg@gmail.com>
2021-11-15 21:49:21 -0500

    updated auth config

--- app/javascript/packs/app/config.js
+++ app/javascript/packs/app/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  client_id: 'rS1c-umx7lO-UuombjIiQdJ7X17u543K8UE0xwPDYVk',
-  client_secret: 'OpTPX1DIghCLwkBxhYe-Hdf_fXIp0Wm0GpoeqxtthN0'
+  client_id: 'AXoJjjNpTnClNx3PTnfnMC4HY5RcRCiVXDre1o5JlOo',
+  client_secret: 'U7vntxLqEOMSiTQBFb6gPSZhy70XW7yBSylUxJL9YdE'
 };
